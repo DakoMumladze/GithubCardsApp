affe80553fa7ce27775e4c0e1a36da4e0118ef36
DakoMumladze <dako.mumladze@geolab.edu.ge>
2019-10-06 18:02:24 +0400

    Displayed test data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
 
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+//CardList
+const CardList = (props) => (
+  <div>
+    {testData.map(profile  =>  <Card {...profile} />)}
+  </div>
+);
+
 class Card extends React.Component {
   render() {
+    const profile = this.props;
     return (
       <div className="github-profile">
-        <img src="https://placehold.it/75" alt="placeholder"/>
+        <img src={profile.avatar_url}alt="placeholder"/>
         <div className="info">
-          <div className="name">Name here...</div>
-          <div className="company">Company here...</div>
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
         </div>
       </div>
     );
@@ -19,7 +33,7 @@ class App extends React.Component {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <Card />
+        <CardList />
       </div>
     )
   }
